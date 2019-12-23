083f97c4aafef23d9cabf048a6974ac1dc81f62a
league <league@imac-12.attlocal.net>
2019-12-22 17:28:36 -0800

    IM STUF

--- src/_02_gotta_catchem_all/ExceptionMethods.java
+++ src/_02_gotta_catchem_all/ExceptionMethods.java
@@ -1,5 +1,10 @@
 package _02_gotta_catchem_all;
 
 public class ExceptionMethods {
-	
+	public static void divide(double num, double denom) {
+		
+	}
+	public static void reverseString(String s) {
+		
+	}
 }
